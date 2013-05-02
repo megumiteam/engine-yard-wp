4b1d445d610b61eb697475bb0f758fad940f396e
wokamoto <wokamoto1973@gmail.com>
2013-05-02 17:59:07 +0900

    added Nginx Hacks plugin

---
+++ wp-content/mu-plugins/nginx_hacks.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Nginx Hacks
+Author: wokamoto
+Plugin URI: http://ninjax.cc/
+Description: Nginx Hacks
+Version: 0.0.1
+Author URI: http://ninjax.cc/
+*/
+
+add_filter('got_rewrite','__return_true');
