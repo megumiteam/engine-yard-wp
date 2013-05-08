24b9c4f7672c0c28d95a41f02ad5441876e2b98a
wokamoto <wokamoto1973@gmail.com>
2013-05-08 12:02:17 +0900

    modified 'Nginx Hacks' plugin

--- wp-content/mu-plugins/nginx_hacks.php
+++ wp-content/mu-plugins/nginx_hacks.php
@@ -4,8 +4,19 @@ Plugin Name: Nginx Hacks
 Author: wokamoto
 Plugin URI: http://ninjax.cc/
 Description: Nginx Hacks
-Version: 0.0.1
+Version: 0.0.2
 Author URI: http://ninjax.cc/
 */
 
 add_filter('got_rewrite','__return_true');
+
+function auto_reverse_proxy_pre_comment_user_ip() {
+	if ( isset($_SERVER['HTTP_X_FORWARDED_FOR']) ) {
+		$X_FORWARDED_FOR = explode(",", $_SERVER['HTTP_X_FORWARDED_FOR']);
+		$REMOTE_ADDR=trim($X_FORWARDED_FOR[0]);
+	} else {
+		$REMOTE_ADDR=$_SERVER['REMOTE_ADDR'];
+	}
+	return $REMOTE_ADDR;
+}
+add_filter ( 'pre_comment_user_ip','auto_reverse_proxy_pre_comment_user_ip');
